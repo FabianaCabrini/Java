e9dbc029b1cfe40f942492305ec88904413ce8fe
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-02 17:36:30 -0300

    Operadores

---
+++ src/main/java/org/example/Operadores.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+    int n1 = 3;
+    int n2 = 5;
+    float m = (n1+ n2)/2;
+
+    System.out.println("A média é igual a "+ m);
+
+
+    }
+}
