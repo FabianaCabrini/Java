c8cf1b5885c709b124d21cc2d1581bcf36036de8
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-05 08:56:26 -0300

    Class_math

---
+++ src/main/java/org/example/Class_math.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Class_math {
+    public static void main(String[] args) {
+
+
+        float v = 8.9f;
+        int ar = (int) Math.ceil(v); //Math.ceil é para arredondar
+        System.out.println(ar);
+
+        //double ale = Math.random(); //Gerar números aleatórios entre 0 e 1
+        //System.out.println(ale);
+
+        double ale =  Math.random();
+        int n = (int) (5 + ale * (10-5)); //Para fazer um número aleatório entre 5 e 10
+        System.out.println(n);
+
+        double sortido = Math.random();
+        int n2 = (int) (70 + sortido * (100-70));
+        System.out.println(n2);
+    }
+}
