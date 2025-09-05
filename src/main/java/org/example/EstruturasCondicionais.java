5407139e674d1753187f37f22742dbbdc09eff33
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-05 16:11:29 -0300

    EstruturaCondicionais

---
+++ src/main/java/org/example/EstruturasCondicionais.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Insira a primeira nota: ");
+        float nota1 = teclado.nextFloat();
+        System.out.print("Insira a segunda nota: ");
+        float nota2 = teclado.nextFloat();
+        float m = (nota1+nota2)/2;
+        System.out.println("Sua média foi "+m);
+        if (m>9){
+            System.out.println("Parabéns");
+        }
+
+        int ano;
+        int nasc;
+        int idade;
+
+        System.out.print("Insira o ano atual: ");
+        ano = teclado.nextInt();
+        System.out.print("Insira o seu ano de nascimento: ");
+        nasc = teclado.nextInt();
+
+        idade = ano - nasc;
+
+        if(idade >=  18){
+            System.out.println("Maior de idade "+idade+" anos");
+        }else {
+            System.out.println("Menor de idade "+idade+" anos");
+        }
+    }
+}
