c476070bd1bf3c6de816de6d08d8423c274d243f
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-08 19:11:22 -0300

    Vetores02

---
+++ src/main/java/org/example/Vetores.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Vetores {
+    public static void main(String[] args) {
+
+     int n[] = {3,5,4,6,9};
+     System.out.println("O total de casas de N " + n.length);
+     for(int c=0; c<=5; c++){
+        System.out.println("Na posição " + c + " temos o valor "+ n[c]);
+       }
+    }
+}
