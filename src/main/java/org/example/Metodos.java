c97c811120df67ebcbd2ba84e9a2c58d9db23c46
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-08 20:13:03 -0300

    metodos

---
+++ src/main/java/org/example/Metodos.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Metodos {
+
+     static void soma(int a, int b){
+     int s = a + b;
+        System.out.println("A soma dos valores é: " + s);
+    }
+     static int subtracao(int c, int d){//necessita colocar o static antes
+        int sub = c - d;
+        return sub; // forma de retornar o valor
+    }
+    public static void main(String[] args){
+        System.out.println("- - - Começou o programa - - -");
+        soma(5,2);
+
+        int sbt = subtracao(4,7); //a nova variavel recebe subtração, e então voce chama ela no println
+        System.out.println("A subtração vale: " + sbt);
+    }
+}
