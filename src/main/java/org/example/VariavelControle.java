11678904890046f8abe3d0833b362c5374d2ee1e
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-08 18:34:20 -0300

    subtrair

--- src/main/java/org/example/VariavelControle.java
+++ src/main/java/org/example/VariavelControle.java
@@ -5,5 +5,8 @@ public class VariavelControle {
         for (int cc=1; cc<=3; cc++){
            System.out.println("Faça");
         }
+        for(int i = 15; i >=5; i-=2){ //decremento de 2  em 2
+            System.out.println(i);
+        }
     }
 }
