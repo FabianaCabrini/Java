485190f86d43c6020a6cd176d6c4058b55372d10
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-10 07:39:59 -0300

    Calculadora

---
+++ src/main/java/org/example/Aulas_Dio/OperadoresAritmeticos.java
@@ -0,0 +1,58 @@
+package org.example.Aulas_Dio;
+
+import java.util.Scanner;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+        int num=1;
+
+        System.out.println("--- CALCULADORA ---");
+        System.out.println("\nEsolha um número\n1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Resto\n0 - Para sair\n");
+        while (num != 0){
+
+           System.out.println("\nEscolha a opção: ");
+           num = scanner.nextInt();
+           switch (num){
+               case 1:
+                   System.out.println("\n--- SOMA ---");
+                   System.out.print("Digite um valor: ");
+                   var value1 = scanner.nextInt();
+                   System.out.print("Digite um segundo valor: ");
+                   var value2 = scanner.nextInt();
+                   System.out.printf("%s + %s = %s\n", value1, value2, value1 + value2);
+                   break;
+               case 2:
+                   System.out.println("\n---SUBTRAÇÃO---");
+                   System.out.print("Digite um valor: ");
+                   var value3 = scanner.nextInt();
+                   System.out.print("Digite um segundo valor: ");
+                   var value4 = scanner.nextInt();
+                   System.out.printf("%s - %s = %s\n", value3, value4, value3 - value4);
+                   break;
+               case 3:
+                   System.out.println("\n---DIVISÃO---");
+                   System.out.print("Digite um valor: ");
+                   var value5 = scanner.nextFloat();
+                   System.out.print("Digite um segundo valor: ");
+                   var value6 = scanner.nextFloat();
+                   System.out.printf("%s / %s = %s\n", value5, value6, value5 / value6);
+                   break;
+               case 4:
+                   System.out.println("\n---RESTO---");
+                   System.out.print("Digite um valor: ");
+                   var value7 = scanner.nextInt();
+                   System.out.print("Digite um segundo valor: ");
+                   var value8 = scanner.nextInt();
+                   System.out.printf("%s %% %s = %s\n", value7, value8, value7 % value8);
+                   break;
+               default:
+                   System.out.println("Opção não encontrada.");
+
+
+
+           }
+       }
+    }
+}
