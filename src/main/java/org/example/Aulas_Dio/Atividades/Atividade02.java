306f16c89c4306ef46c31d5628cd887b96cbeb4d
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-10 15:17:34 -0300

    atividade02

--- src/main/java/org/example/Aulas_Dio/Atividades/Atividade02.java
+++ src/main/java/org/example/Aulas_Dio/Atividades/Atividade02.java
@@ -13,7 +13,10 @@ public class Atividade02 {
         age = new Scanner (System.in).nextByte();
 
         if(age >= 18){
+            System.out.println("--- BEM VINDO ---");
             System.out.printf("%s, sua idade é %d. Então você está apto a dirigir\n",name,age);
+        }else {
+            System.out.printf("%s, sua idade é inferior a 18 anos, infelizmente não esta apto a dirigir\n",name);
         }
         System.out.println("Fim do programa");
 
