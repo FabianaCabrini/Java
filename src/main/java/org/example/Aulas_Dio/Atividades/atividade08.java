1f4f07e03defa430326464411e0390f26a179fc2
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-11 20:26:47 -0300

    Atividade06

--- src/main/java/org/example/Aulas_Dio/Atividades/atividade08.java
+++ src/main/java/org/example/Aulas_Dio/Atividades/atividade08.java
@@ -12,5 +12,32 @@ public class atividade08 {
         de números informados, incluindo os números informados e em ordem decrescente;
          */
 
+        /*System.out.print("Digite um número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite um segundo número: ");
+        int num2 = scanner.nextInt();
+        System.out.print("\n--- IMPAR X PAR ---");
+        System.out.print("Esolha\n1 - para Impar\n2 - para Par");
+        var op = scanner.nextInt();
+        switch (op){
+            case 1:
+                while ()
+        }*/
+        int num;
+        System.out.print("--- IMPAR X PAR ---\n");
+        System.out.print("Digite um número: ");
+        num = scanner.nextInt();
+        while (num !=0){
+            System.out.print("Digite um número: ");
+            num = scanner.nextInt();
+
+            if(num % 2 == 0){
+                System.out.println("Pares");
+                System.out.println(num);
+            }else{
+                System.out.println("Impar");
+                System.out.println(num);
+            }
+        }
     }
 }
