2a703d087509dc9e107fdbf1a5c3d3751ca40b30
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-15 18:56:48 -0300

    looping infinito

--- src/main/java/org/example/Aulas_Dio/Imports.java
+++ src/main/java/org/example/Aulas_Dio/Imports.java
@@ -9,7 +9,9 @@ public class Imports {
     public static void main(String[] args) {
 
         Random generate = new Random(); //criarmos números aleatórios
-
+        /*
+        Para criarmos um laço infinito em for é só fazer for(;;)
+         */
         for(int i = 0; i < 6; i++){
 
             int number = generate.nextInt(60);
