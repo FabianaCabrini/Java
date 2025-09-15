46e4b69128399cc50630b0d344354cce5a775550
Fabiana Cabrini <fabianacabrini04@gmail.com>
2025-09-15 20:29:36 -0300

    vibilidade

--- src/main/java/org/example/Conceitos_Base/Visibilidade.java
+++ src/main/java/org/example/Conceitos_Base/Visibilidade.java
@@ -1,4 +1,15 @@
 package org.example.Conceitos_Base;
 
 public class Visibilidade {
+
+    //padrão (default) não definindo nada
+
+    //public (visibilidade publica para  todos os packages)
+
+    //private - visibilidade privada para todos os packages
+
+    public static void main(String[] args) {
+        //Public - visibilidade de escopo do projeto
+
+    }
 }
